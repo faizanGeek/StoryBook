c86569d7adf5118437caed71e3bd932c5dad569f
faizanGeek <thegreedy28@gmail.com>
2019-03-16 21:20:17 +0530

    sociofy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:'mongodb://faizan_7417:the123454321@ds161144.mlab.com:61144/storybooks-dev',
-  googleClientID:'365919299983-60ht41q8hieshf6olgnf5k1o1hidj16g.apps.googleusercontent.com',
-  googleClientSecret:'1yLf12qkATG3NaDemeqEhTh-'
+  googleClientID:'566657226933-jam2hvuhclshmls3ng5q5c6dmrc2si8j.apps.googleusercontent.com',
+  googleClientSecret:'DlPJEZzp0Q6alM5lW2lVspco'
 }
